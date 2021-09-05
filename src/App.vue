430976291a3de10c73166c1669035e7e55dc5ee2
Justin Shilling <loveofmusic25@hotmail.com>
2021-09-05 10:13:16 -0400

    images, config, root styles, cleanup

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,8 @@
 </template>
 
 <script>
-import TheHeader from "@/components/TheHeader.vue";
-import TheFooter from "@/components/TheFooter.vue";
+import TheHeader from "@/components/layout/TheHeader.vue";
+import TheFooter from "@/components/layout/TheFooter.vue";
 
 export default {
     components: {
