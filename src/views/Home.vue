d69898633a726513976e27569f88f1da690156b3
Justin Shilling <loveofmusic25@hotmail.com>
2021-09-06 16:00:19 -0400

    form submission update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -136,7 +136,6 @@
                         class="contactForm"
                         data-netlify="true"
                         data-netlify-honeypot="bot-field"
-                        @submit.prevent="onFormSubmit"
                     >
                         <input
                             type="hidden"
@@ -148,6 +147,7 @@
                             <TextInput
                                 v-model="contactForm.name"
                                 name="name"
+                                required
                             />
                         </InputGroup>
                         <InputGroup>
@@ -155,6 +155,7 @@
                             <TextInput
                                 v-model="contactForm.email"
                                 name="email"
+                                required
                             />
                         </InputGroup>
                         <InputGroup>
@@ -162,8 +163,11 @@
                             <TextBox
                                 v-model="contactForm.message"
                                 name="message"
+                                required
                             />
                         </InputGroup>
+
+
 
                         <AppButton
                             type="submit"
