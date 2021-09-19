79d7e2408635ac8e6a87d93ed57de8f551310169
Justin Shilling <loveofmusic25@hotmail.com>
2021-09-19 15:49:58 -0400

    section component adjustments & additions

---
+++ src/components/layout/section/SectionCopy.vue
@@ -0,0 +1,32 @@
+<template>
+    <p :style="{
+        marginBottom: mb,
+        width: width
+    }">
+        <slot></slot>
+    </p>
+</template>
+<script>
+export default {
+    props: {
+        mb: {
+            type: String,
+            default: "",
+        },
+        width: {
+            type: String,
+            default: "100%",
+        },
+    },
+};
+</script>
+<style lang="scss">
+p {
+    color: #fff;
+    font-weight: 200;
+    font-size: 1.8rem;
+    @include breakpoint(tablet-land) {
+        text-align: center;
+    }
+}
+</style>
