ad6f5149d2bfcff6aec5967fd0ea1e168076d7e1
Justin Shilling <loveofmusic25@hotmail.com>
2023-02-14 09:41:17 -0500

    temp page

--- app.vue
+++ app.vue
@@ -1,15 +1,19 @@
 <template>
   <TheHeader
+    v-if="$route.path !== '/surprise'"
     class="outer-container"
     :show-mobile-menu="showMobileMenu || windowWidth > 768"
     @open="showMobileMenu = true"
     @close="showMobileMenu = false"
   />
   <NuxtPage />
-  <TheFooter />
+  <TheFooter v-if="$route.path !== '/surprise'" />
 </template>
 
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const $route = useRoute();
 const showMobileMenu = ref<boolean>(false);
 const windowWidth = ref<number>(document.documentElement.clientWidth);
 
@@ -21,6 +25,7 @@ onBeforeMount(() => {
     window.addEventListener("resize", getWindowWidth);
   });
 });
+console.log($route.path);
 </script>
 
 <style lang="scss"></style>
